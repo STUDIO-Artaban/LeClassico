c0b358990a2f8e10d6bca82f5de9319355b7ef78
v-pascal <pascal.viguie@studio-artaban.com>
2017-03-05 13:48:44 +0100

    Insert & Delete operations added

--- WebServices/abonnements.php
+++ WebServices/abonnements.php
@@ -6,6 +6,9 @@ $Clf = $_GET['Clf'];
 $Ope = $_GET['Ope'];
 $StatusDate = $_GET['StatusDate'];
 
+$Keys = $_POST['Keys'];
+$Status = $_POST['Status'];
+
 header('Content-Type: application/json;charset=ISO-8859-1');
 
 if (!Empty($Clf)) {
@@ -23,8 +26,97 @@ if (!Empty($Clf)) {
 
             mysql_free_result($Result);
             switch ($Ope) {
-                case 1:
-                case 2: { ////// Select
+
+                case 4: ////// Insert
+                case 5: { ////// Delete
+
+                    if (Empty($Keys)) {
+                        echo '{"Error":'.strval(constant("WEBSERVICE_ERROR_MISSING_KEYS")).'}';
+                        break;
+                    }
+                    if (Empty($Status)) {
+                        echo '{"Error":'.strval(constant("WEBSERVICE_ERROR_MISSING_STATUS")).'}';
+                        break;
+                    }
+                    $Keys = json_decode($Keys, true);
+                    if (json_last_error() != JSON_ERROR_NONE) {
+                        echo '{"Error":'.strval(constant("WEBSERVICE_ERROR_INVALID_KEYS")).'}';
+                        break;
+                    }
+                    $Status = json_decode($Status, true);
+                    if (json_last_error() != JSON_ERROR_NONE) {
+                        echo '{"Error":'.strval(constant("WEBSERVICE_ERROR_INVALID_STATUS")).'}';
+                        break;
+                    }
+
+                    //////
+                    $i = 0;
+                    $Lenght = count($Keys);
+                    if ($Ope == 4) {
+
+                        for ( ; $i < $Lenght; ++$i) { // Insert loop
+
+                            $Query = "INSERT INTO Abonnements (ABO_Pseudo,ABO_Camarade) VALUES ('";
+                            $Query .= addslashes($Keys[$i]['Pseudo'])."','".addslashes($Keys[$i]['Camarade'])."')";
+                            if (!mysql_query(trim($Query),$Link)) {
+                                
+                                $Query = "UPDATE Abonnements SET";
+                                $Query .= " ABO_Status = 1,";
+                                $Query .= " ABO_StatusDate = CURRENT_TIMESTAMP";
+
+                                $Query .= " WHERE";
+                                $Query .= " ABO_Pseudo='".addslashes($Keys[$i]['Pseudo'])."' AND";
+                                $Query .= " ABO_Camarade='".addslashes($Keys[$i]['Camarade'])."' AND";
+                                $Query .= " ABO_Status=2 AND";
+                                $Query .= " ABO_StatusDate < '".trim($Status[$i]['StatusDate'])."'";
+                                if (!mysql_query(trim($Query),$Link)) {
+                                    echo '{"Error":'.strval(constant("WEBSERVICE_ERROR_QUERY_INSERT")).'}';
+                                    break;
+                                }
+                                if ((mysql_affected_rows() == 0) && ((is_null($StatusDate)) || (strcmp($StatusDate, $Status[$i]['StatusDate']) < 0)))
+                                    $StatusDate = $Status[$i]['StatusDate'];
+                                    // NB: Needed to return records updated after current request
+                            }
+                        }
+
+                    } else {
+                        for ( ; $i < $Lenght; ++$i) { // Delete loop
+
+                            $Query = "UPDATE Abonnements SET";
+                            $Query .= " ABO_Status = 2,";
+                            $Query .= " ABO_StatusDate = CURRENT_TIMESTAMP";
+
+                            $Query .= " WHERE";
+                            $Query .= " ABO_Pseudo='".addslashes($Keys[$i]['Pseudo'])."' AND";
+                            $Query .= " ABO_Camarade='".addslashes($Keys[$i]['Camarade'])."' AND";
+                            $Query .= " ABO_StatusDate < '".trim($Status[$i]['StatusDate'])."'";
+                            if (!mysql_query(trim($Query),$Link)) {
+                                echo '{"Error":'.strval(constant("WEBSERVICE_ERROR_QUERY_DELETE")).'}';
+                                break;
+                            }
+                            if (mysql_affected_rows() == 0) {
+
+                                $Query = "INSERT INTO Abonnements (ABO_Pseudo,ABO_Camarade,ABO_Status) VALUES ('";
+                                $Query .= addslashes($Keys[$i]['Pseudo'])."','".addslashes($Keys[$i]['Camarade'])."',2)";
+                                if (!mysql_query(trim($Query),$Link)) {
+
+                                    if ((is_null($StatusDate)) || (strcmp($StatusDate, $Status[$i]['StatusDate']) < 0))
+                                        $StatusDate = $Status[$i]['StatusDate'];
+                                        // NB: Needed to return records updated after current request
+                                }
+                            }
+                        }
+                    }
+                    if ($i != $Lenght)
+                        break; // Error
+
+                    if (is_null($StatusDate))
+                        $StatusDate = date("Y-m-d H:i:s", strtotime(getTimeStamp($Link)) - 1);
+
+                    // Let's reply with inserted or deleted records
+                    //break;
+                }
+                case 1: { ////// Select
 
                     $Query = "SELECT * FROM Abonnements WHERE UPPER(ABO_Pseudo) = UPPER('".addslashes($Camarade)."')";
                     if ((!is_null($StatusDate)) && (strcmp(trim($StatusDate),"")))
@@ -49,18 +141,6 @@ if (!Empty($Clf)) {
                         $Reply .= ']}';
                     }
                     echo $Reply;
-                    break;
-                }
-                case 3: { ////// Update
-
-                    break;
-                }
-                case 4: { ////// Insert
-
-                    break;
-                }
-                case 5: { ////// Delete
-
                     break;
                 }
                 default: {
